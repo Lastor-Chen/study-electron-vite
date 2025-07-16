a6afa757ce20066c6aedbb6f38bce8afc84d6e74
Lastor <lastorchen@ikv.tw>
2025-07-16 14:36:28 +0800

    build(vep): preload 編譯為 .cjs

--- apps/vite-electron-plugin/vite.config.ts
+++ apps/vite-electron-plugin/vite.config.ts
@@ -1,18 +1,19 @@
-import { rmSync } from 'node:fs'
-import { resolve } from 'node:path'
+import fs from 'node:fs'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-electron-plugin'
+import { dest } from 'vite-electron-plugin/plugin'
 
 // clear outDir before build
-rmSync('dist-electron', { recursive: true, force: true })
+fs.rmSync('dist-electron', { recursive: true, force: true })
 
 export default defineConfig(() => {
   return {
     resolve: {
       alias: {
-        '@shared': resolve(import.meta.dirname, './shared'),
+        '@shared': path.resolve(import.meta.dirname, './shared'),
       },
     },
     plugins: [
@@ -23,7 +24,25 @@ export default defineConfig(() => {
           format: 'esm',
           target: 'node22',
         },
+        plugins: [
+          dest((from, to) => {
+            // rename preload extname
+            if (from.includes('preload')) {
+              to = renameExtTo(to!, '.cjs')
+            }
+
+            return to
+          }),
+        ],
       }),
     ],
   }
 })
+
+function renameExtTo(filePath: string, ext: string) {
+  const parsed = path.parse(filePath)
+  parsed.base = '' // clear basename to avoid overwrite
+  parsed.ext = ext
+  
+  return path.format(parsed)
+}
