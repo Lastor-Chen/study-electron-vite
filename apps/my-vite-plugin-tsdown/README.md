418b9b0cedb292f2827274eaaa64851f35b8107e
Lastor <lastorchen@ikv.tw>
2025-11-07 19:08:05 +0800

    build(myp): 處理 vite.config 熱更新會重複開 electron 的問題

--- apps/my-vite-plugin-tsdown/README.md
+++ apps/my-vite-plugin-tsdown/README.md
@@ -8,7 +8,12 @@
 - 程序性使用 vs config 的行為不明，似乎最好是禁用 config 避免干擾
 - 多 build thread 時 onSuccess, watch 等機制是各自獨立的
   - 如果各自 watch, 因為 main thread 要忽略 preload, 設定繁瑣
-  - 統一 watch 則會有 onSuccess 被觸發兩次的問題
+  - 統一 watch 則會有 onSuccess 被觸發兩次的問題, 但可只掛一個來解決
+- 拉起 electron 時，不下 `--no-sandbox` 會無法 restart
+- tsdown onSuccess 可以直接跑 `$ electron .` 但有缺點
+  - 無法監控其 exit 後關閉 vite
+  - 若是 vite config change 會殘留殭屍進程
+- vite config change 時，是 reload 不是 restart，子程序變數會遺失，但可改存在 global process
 
 ## TODO
 
--- apps/my-vite-plugin-tsdown/plugins/vite-plugin-tsdown/electronStartup.ts
+++ apps/my-vite-plugin-tsdown/plugins/vite-plugin-tsdown/electronStartup.ts
@@ -3,7 +3,13 @@ import type { ChildProcess, SpawnOptions } from 'node:child_process'
 
 import electronPath from 'electron'
 
-let electronProc: ChildProcess | null = null
+declare global {
+  namespace NodeJS {
+    interface Process {
+      electronProc: ChildProcess | undefined
+    }
+  }
+}
 
 /**
  * electron argv path 給 `.` 會以 packageJson.main 作為進入點
@@ -15,23 +21,26 @@ export async function spawnElectron(
 ) {
   await killElectronIfExist()
 
-  electronProc = spawn(electronPath as unknown as string, argv, {
+  // 存到 global process 上, 避免 vite config 熱更新時丟失
+  process.electronProc = spawn(electronPath as unknown as string, argv, {
     stdio: 'inherit',
     ...options,
   })
 
-  electronProc.once('close', () => {
+  process.electronProc.once('close', () => {
     console.log('electron process closed')
     process.exit()
   })
 }
 
 async function killElectronIfExist() {
-  if (electronProc && !electronProc.killed) {
-    await new Promise((resolve) => {
-      electronProc!.removeAllListeners() // 避免 restart 觸發 onClose
-      electronProc!.once('exit', resolve)
-      electronProc!.kill()
-    })
-  }
+  await new Promise<void>((resolve) => {
+    if (process.electronProc && !process.electronProc.killed) {
+      process.electronProc.removeAllListeners() // 避免 restart 觸發 onClose
+      process.electronProc.once('exit', resolve)
+      process.electronProc.kill()
+    } else {
+      resolve()
+    }
+  })
 }
